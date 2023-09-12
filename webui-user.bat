5a3cac6af4772507420d7134be0da7fa56d3c298
Lei Zhang <1023086065@qq.com>
2023-09-13 01:11:15 +0800

    update

--- webui-user.bat
+++ webui-user.bat
@@ -1,5 +1,15 @@
 @echo off
 
+git pull
+cd extensions
+cd sd-webui-controlnet
+git pull
+cd ..
+cd stable-diffusion-webui-images-browser
+git pull
+cd ..
+cd ..
+
 set PYTHON="C:\Software\Python10\python.exe"
 set GIT=
 set VENV_DIR=
